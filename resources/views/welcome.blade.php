0c62e4b40c894858f9187bb61dd4843f605a118e
Jeremy Anderson <529446+defenestrator@users.noreply.github.com>
2021-07-01 07:48:09 -0600

    comment out auth links

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -403,7 +403,7 @@
 <body class="antialiased">
     <div
         class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-        @if (Route::has('login'))
+        {{-- @if (Route::has('login'))
             <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                 @auth
                     <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
@@ -415,7 +415,7 @@
                     @endif
                 @endauth
             </div>
-        @endif
+        @endif --}}
 
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
             <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
