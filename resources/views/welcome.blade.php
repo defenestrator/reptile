8d17deec03cff87a4f6e9683703eef45d794d156
Jeremy Anderson <529446+defenestrator@users.noreply.github.com>
2021-07-03 07:08:18 -0600

    better

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,12 +3,18 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1"
+    >
 
     <title>Reptile Bio</title>
 
     <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap"
+        rel="stylesheet"
+    >
 
     <!-- Styles -->
     <style>
@@ -420,7 +426,11 @@
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
             <div class="flex justify-center pt-8 sm:justify-start">
                 <div class="block items-top justify-center w-50 mt-8 mx-2 px-2">
-                    <img src="rb-logo2.png" class="ml-4 lg:m-0" style="width:80%; max-width:640px;">
+                    <img
+                        src="rb-logo2.png"
+                        class="ml-4 lg:m-0"
+                        style="width:80%; max-width:640px;"
+                    >
                 </div>
             </div>
             <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
@@ -475,13 +485,12 @@
                         <div class="ml-12">
                             <div class="mt-2 text-gray-600 dark:text-gray-400">
                                 Reptile Bio is one tool to help us build a better future for our scaly pets and
-                                ourselves.
-                                Easily keep track of the animals under your care and we will host it and keep it safe
-                                for
-                                the next 100 years. Yes, really.
+                                ourselves. Easily keep track of the animals under your care and we will host it and keep
+                                it safe for the next 100 years. Yes, really.
                                 Our goal is to provide a public stewardship service of captive herpetocultural records
-                                for
-                                at least the next century.
+                                for at least the next century. The first twenty years is on us. Help us build a
+                                healthier culture
+                                within herpetoculture, one that cares about the future, together.
                             </div>
                         </div>
                     </div>
@@ -491,25 +500,47 @@
             <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
                 <div class="text-center text-sm text-gray-500 sm:text-left">
                     <div class="flex items-center">
-                        <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            viewBox="0 0 24 24" stroke="currentColor" class="-mt-px w-5 h-5 text-gray-400">
+                        <svg
+                            fill="none"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            class="-mt-px w-5 h-5 text-gray-400"
+                        >
                             <path
-                                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z">
+                                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                            >
                             </path>
                         </svg>
 
-                        <a href="#" class="ml-1 underline">
+                        <a
+                            href="#"
+                            class="ml-1 underline"
+                        >
                             Shop
                         </a>
 
-                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                            stroke-width="2" viewBox="0 0 24 24" class="ml-4 -mt-px w-5 h-5 text-gray-400">
+                        <svg
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            viewBox="0 0 24 24"
+                            class="ml-4 -mt-px w-5 h-5 text-gray-400"
+                        >
                             <path
-                                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z">
+                                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                            >
                             </path>
                         </svg>
 
-                        <a href="#" class="ml-1 underline">
+                        <a
+                            href="#"
+                            class="ml-1 underline"
+                        >
                             Sponsor
                         </a>
                     </div>
